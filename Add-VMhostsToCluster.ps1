17963b1151a5d06d3a448755924d00ef67510f4a
padams-mdsol <padams@mdsol.com>
2018-03-26 10:30:31 -0500

    Function
    Created as a function.

--- Add-VMhostsToCluster.ps1
+++ Add-VMhostsToCluster.ps1
@@ -1,44 +1,35 @@
-# PowerCLI script to add ESXi hosts to vCenter Server
-# Version 1.0
+# PowerCLI function to add ESXi hosts to vCenter Server
+# Version 1.1
 # 
-# ---------------------------------------------------
-# Edit the information below to match your environment
-#
-# Specify vCenter Server, vCenter Server username, vCenter Server user password, vCenter Server location which can be the Datacenter, a Folder or a Cluster (which I used).
-# Parameterize, take user input instead of hardcoded
-$vCenter="vcenter"
+function Add-VMhostsToCluster {
+    Param(
+        [string]$vCenter,
+        [string]$clusterName,
+        [string[]]$esxihosts
+    )
+  
+        Write-host Enter your vCenter Credentials. -foreground green
+        $vCenterCred = Get-Credential
 
-# Change to Get-Credential
-$vCenterUser=""
-$vCenterUserPassword="not-secret"
+        Write-host Enter your ESXi username and password. -foreground green
+        $esxiCred = Get-Credential  
+        # ---------------------------------------------------
+        #
+        #Connect to vCenter Server
+        write-host Connecting to vCenter Server $vCenter -foreground green
+        Connect-viserver $vCenter -Credential $vCenterCred
+        #
+        write-host --------
+        write-host Start adding ESXi hosts to the vCenter Server $vCenter
+        write-host --------
+        #
+        # Add ESXi hosts
+        foreach ($esxihost in $esxihosts) {
+        Add-VMHost $esxihost -Location $clusterName -Credential $esxiCred -Force
+        } #foreach
 
-# Parameterize this piece
-$vcenterlocation="npx005-01"
-#
-# Specify the ESXi host you want to add to vCenter Server and the user name and password to be used.
-# Paramtererize, take input from pipeline, get-content
-$esxihosts=("host01","host02")
+        # Disconnect from vCenter Server
+        write-host "Disconnecting to vCenter Server $vcenter" -foreground green
+        disconnect-viserver -confirm:$false | out-null
 
-# Change to Get-Credential
-$esxihostuser="root"
-$esxihostpasswd="not-secret"
-#
-# You don't have to change anything below this line
-# ---------------------------------------------------
-#
-#Connect to vCenter Server
-write-host Connecting to vCenter Server $vcenter -foreground green
-Connect-viserver $vCenter -user $vCenterUser -password $vCenterUserPassword -WarningAction 0 | out-null
-#
-write-host --------
-write-host Start adding ESXi hosts to the vCenter Server $vCenter
-write-host --------
-#
-# Add ESXi hosts
-foreach ($esxihost in $esxihosts) {
-Add-VMHost $esxihost -Location $vcenterlocation -User $esxihostuser -Password $esxihostpasswd
-}
-#
-# Disconnect from vCenter Server
-write-host "Disconnecting to vCenter Server $vcenter" -foreground green
-disconnect-viserver -confirm:$false | out-null
+    } #function
